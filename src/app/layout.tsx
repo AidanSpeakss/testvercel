03194e15c6c39a15a429760526ef08e5d5ac0717
Aidan <24593916+AidanSpeakss@users.noreply.github.com>
2024-06-03 04:34:01 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-      </head>
       <body className="h-screen bg-black text-white overflow-y-scroll font-lato">{children}</body>
     </html>
   );
